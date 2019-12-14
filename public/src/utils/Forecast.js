709d87760437835dcf210c2eaad9abed32115bcf
Priyanka Jacob <priyanka.jacob93@gmail.com>
2019-12-14 23:54:21 +0530

    global module removal dependencies

--- public/src/utils/Forecast.js
+++ public/src/utils/Forecast.js
@@ -4,10 +4,10 @@ const forecastReq = (long,lang,callback)=>{
     const url = `https://api.darksky.net/forecast/058170a18732f24bf65d5ffed42afcba/${long},${lang}`
     request({url,json:true},(error,{body})=>{
         if(error){
-            callback({error:'unable to connect to dark sky api'},undefined)
+            callback({error:'Unable to connect to dark sky api'},undefined)
         }
         else if(body.error){
-            callback({error:'unable to fetch temp & precip'},undefined)
+            callback({error:'Unable to fetch temp & precip'},undefined)
         }
         else {
             const {icon:forecast}=body.currently
